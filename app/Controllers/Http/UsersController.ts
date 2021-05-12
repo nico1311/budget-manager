176ba7e2e98b919970dc9837836b15c284df9af7
nico1311 <71448116+nico1311@users.noreply.github.com>
2021-05-12 19:16:58 -0300

    Implement user registration

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,10 +1,11 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import { rules, schema } from '@ioc:Adonis/Core/Validator';
+import User from 'App/Models/User';
 
 export default class UsersController {
   public async getCurrentUserInfo(ctx: HttpContextContract) {
-    const { auth } = ctx;
-
-    const user = auth.user!;
+    const { auth } = ctx,
+      user = auth.user!;
 
     return {
       id: user.id,
@@ -14,11 +15,33 @@ export default class UsersController {
   }
 
   public async createUser(ctx: HttpContextContract) {
-    const { auth } = ctx;
+    const { auth, request, response } = ctx;
 
-    if (auth.user) throw new Error('Cannot create a new user while authenticated')
+    const createUserPayloadSchema = schema.create({
+      name: schema.string({trim: true}),
+      email: schema.string({trim: true}, [
+        rules.email(),
+        rules.unique({
+          table: 'users',
+          column: 'email',
+          caseInsensitive: true
+        })
+      ]),
+      password: schema.string({}, [
+        rules.confirmed()
+      ])
+    });
 
-    return { success: true }
+    const userDetails = await request.validate({schema: createUserPayloadSchema});
+    const user = await User.create(userDetails);
+
+    await auth.use('web').login(user);
+
+    response.status(201).send({
+      id: user.id,
+      email: user.email,
+      name: user.name
+    });
   }
 
 }
