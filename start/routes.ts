62788100d8b8b271fa3812a327713df1cd73a89b
nico1311 <71448116+nico1311@users.noreply.github.com>
2021-05-19 02:25:18 -0300

    some changes for serving frontend

--- start/routes.ts
+++ start/routes.ts
@@ -1,17 +1,13 @@
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async () => {
-  return { hello: 'world' }
-});
+Route.post('/api/auth/login', 'AuthController.login');
+Route.post('/api/auth/logout', 'AuthController.logout').middleware('auth');
 
-Route.post('/auth/login', 'AuthController.login');
-Route.post('/auth/logout', 'AuthController.logout').middleware('auth');
+Route.post('/api/users', 'UsersController.createUser');
+Route.get('/api/users/me', 'UsersController.getCurrentUserInfo').middleware('auth');
 
-Route.post('/users', 'UsersController.createUser');
-Route.get('/users/me', 'UsersController.getCurrentUserInfo').middleware('auth');
-
-Route.post('/transactions', 'TransactionsController.createTransaction').middleware('auth');
-Route.get('/transactions', 'TransactionsController.listTransactions').middleware('auth');
-Route.get('/transactions/:id', 'TransactionsController.getTransaction').middleware('auth');
-Route.patch('/transactions/:id', 'TransactionsController.editTransaction').middleware('auth');
-Route.delete('/transactions/:id', 'TransactionsController.deleteTransaction').middleware('auth');
+Route.post('/api/transactions', 'TransactionsController.createTransaction').middleware('auth');
+Route.get('/api/transactions', 'TransactionsController.listTransactions').middleware('auth');
+Route.get('/api/transactions/:id', 'TransactionsController.getTransaction').middleware('auth');
+Route.patch('/api/transactions/:id', 'TransactionsController.editTransaction').middleware('auth');
+Route.delete('/api/transactions/:id', 'TransactionsController.deleteTransaction').middleware('auth');
